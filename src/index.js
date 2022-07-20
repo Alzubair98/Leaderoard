fe3cd2b7b0e6cfa163c383943e8efb52b2621452
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2022-07-21 00:32:01 +0300

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,55 +1,51 @@
 import './style.css';
 
-let form = document.querySelector('.form');
-let name = document.querySelector('.name_input');
-let score = document.querySelector('.score_input');
+const form = document.querySelector('.form');
+const name = document.querySelector('.name_input');
+const score = document.querySelector('.score_input');
 
 const setEndPoint = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/epd1eVyK5Nyiug7lZWsg/scores/';
 
-const addScore = ()=>{
-    fetch(setEndPoint, {
+const addScore = () => {
+  fetch(setEndPoint, {
     method: 'POST',
-    headers:{
-        'Content-type': 'application/json; charset=UTF-8'},
-        body: JSON.stringify({
-            user: name.value,
-            score: score.value,
-        }),
-    },
-)};
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+    body: JSON.stringify({
+      user: name.value,
+      score: score.value,
+    }),
+  });
+};
 
-let List = document.querySelector(".scores-list")
+const List = document.querySelector('.scores-list');
 
 const getScore = () => {
-    fetch(setEndPoint).then(async(res)=>{
-        const data = await res.json();
-        const arrayOfResults = data.result;
-        return arrayOfResults;
-    })
+  fetch(setEndPoint).then(async (res) => {
+    const data = await res.json();
+    const arrayOfResults = data.result;
+    return arrayOfResults;
+  })
     .then((arrayOfResults) => {
-        for(let i = 0; i < arrayOfResults.length; i +=1){
-            const newscore = document.createElement('li');
-            newscore.textContent = arrayOfResults[i].user + ' : ' + arrayOfResults[i].score;
-            List.appendChild(newscore)
-        }
+      for (let i = 0; i < arrayOfResults.length; i += 1) {
+        const newscore = document.createElement('li');
+        newscore.textContent = `${arrayOfResults[i].user} : ${arrayOfResults[i].score}`;
+        List.appendChild(newscore);
+      }
     });
-}
-
+};
 
 getScore();
 
+const refreshButton = document.querySelector('.refresh-button');
 
-let refreshButton = document.querySelector('.refresh-button')
+refreshButton.addEventListener('click', () => {
+  List.innerHTML = '';
+  getScore();
+});
 
-refreshButton.addEventListener('click', ()=>{
-    List.innerHTML = '';
-    getScore();
-})
-
-form.addEventListener('submit', (e)=> {
-    e.preventDefault();
-    addScore();
-    name.value = '';
-    score.value = '';
-})
-
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addScore();
+  name.value = '';
+  score.value = '';
+});
